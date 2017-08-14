e237c2c1dd26262bf0cff820714de3516d953b81
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-08-14 16:09:41 -0500

    Fix indenting

--- impact_effort_scatter_plot.py
+++ impact_effort_scatter_plot.py
@@ -1,306 +1,57 @@
-%!PS-Adobe-3.0
-%%Title: impact_effort_scatter_plot.py
-%%For: gilles
-%%Creator: NVIM v0.2.0
-%%CreationDate: Tue Aug  8 16:06:38 2017
-%%DocumentData: Clean8Bit
-%%Orientation: Portrait
-%%Pages: (atend)
-%%PageOrder: Ascend
-%%BoundingBox: 59 49 564 800
-%%DocumentMedia: A4 595 842 0 () ()
-%%DocumentNeededResources: font Courier
-%%+ font Courier-Bold
-%%+ font Courier-Oblique
-%%+ font Courier-BoldOblique
-%%DocumentSuppliedResources: procset VIM-Prolog 1.4 1
-%%+ encoding VIM-latin1 1.0 0
-%%Requirements: duplex collate color
-%%EndComments
-%%BeginDefaults
-%%PageResources: font Courier
-%%+ font Courier-Bold
-%%+ font Courier-Oblique
-%%+ font Courier-BoldOblique
-%%PageMedia: A4
-%%EndDefaults
-%%BeginProlog
-%%BeginResource: procset VIM-Prolog
-%%BeginDocument: /usr/share/nvim/runtime/print/prolog.ps
-%!PS-Adobe-3.0 Resource-ProcSet
-%%Title: VIM-Prolog
-%%Version: 1.4 1
-%%EndComments
-% Editing of this file is NOT RECOMMENDED.  You run a very good risk of causing
-% all PostScript printing from VIM failing if you do.  PostScript is not called
-% a write-only language for nothing!
-/packedarray where not{userdict begin/setpacking/pop load def/currentpacking
-false def end}{pop}ifelse/CP currentpacking def true setpacking
-/bd{bind def}bind def/ld{load def}bd/ed{exch def}bd/d/def ld
-/db{dict begin}bd/cde{currentdict end}bd
-/T true d/F false d
-/SO null d/sv{/SO save d}bd/re{SO restore}bd
-/L2 systemdict/languagelevel 2 copy known{get exec}{pop pop 1}ifelse 2 ge d
-/m/moveto ld/s/show ld /ms{m s}bd /g/setgray ld/r/setrgbcolor ld/sp{showpage}bd
-/gs/gsave ld/gr/grestore ld/cp/currentpoint ld
-/ul{gs UW setlinewidth cp UO add 2 copy newpath m 3 1 roll add exch lineto
-stroke gr}bd
-/bg{gs r cp BO add 4 -2 roll rectfill gr}bd
-/sl{90 rotate 0 exch translate}bd
-L2{
-/sspd{mark exch{setpagedevice}stopped cleartomark}bd
-/nc{1 db/NumCopies ed cde sspd}bd
-/sps{3 db/Orientation ed[3 1 roll]/PageSize ed/ImagingBBox null d cde sspd}bd
-/dt{2 db/Tumble ed/Duplex ed cde sspd}bd
-/c{1 db/Collate ed cde sspd}bd
-}{
-/nc{/#copies ed}bd
-/sps{statusdict/setpage get exec}bd
-/dt{statusdict/settumble 2 copy known{get exec}{pop pop pop}ifelse
-statusdict/setduplexmode 2 copy known{get exec}{pop pop pop}ifelse}bd
-/c{pop}bd
-}ifelse
-/ffs{findfont exch scalefont d}bd/sf{setfont}bd
-/ref{1 db findfont dup maxlength dict/NFD ed{exch dup/FID ne{exch NFD 3 1 roll
-put}{pop pop}ifelse}forall/Encoding findresource dup length 256 eq{NFD/Encoding
-3 -1 roll put}{pop}ifelse NFD dup/FontType get 3 ne{/CharStrings}{/CharProcs}
-ifelse 2 copy known{2 copy get dup maxlength dict copy[/questiondown/space]{2
-copy known{2 copy get 2 index/.notdef 3 -1 roll put pop exit}if pop}forall put
-}{pop pop}ifelse dup NFD/FontName 3 -1 roll put NFD definefont pop end}bd
-CP setpacking
-(\004)cvn{}bd
-% vim:ff=unix:
-%%EOF
-%%EndDocument
-%%EndResource
-%%BeginResource: encoding VIM-latin1
-%%BeginDocument: /usr/share/nvim/runtime/print/latin1.ps
-%!PS-Adobe-3.0 Resource-Encoding
-%%Title: VIM-latin1
-%%Version: 1.0 0
-%%EndComments
-/VIM-latin1[
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/space /exclam /quotedbl /numbersign /dollar /percent /ampersand /quotesingle
-/parenleft /parenright /asterisk /plus /comma /minus /period /slash
-/zero /one /two /three /four /five /six /seven
-/eight /nine /colon /semicolon /less /equal /greater /question
-/at /A /B /C /D /E /F /G
-/H /I /J /K /L /M /N /O
-/P /Q /R /S /T /U /V /W
-/X /Y /Z /bracketleft /backslash /bracketright /asciicircum /underscore
-/grave /a /b /c /d /e /f /g
-/h /i /j /k /l /m /n /o
-/p /q /r /s /t /u /v /w
-/x /y /z /braceleft /bar /braceright /asciitilde /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef
-/space /exclamdown /cent /sterling /currency /yen /brokenbar /section
-/dieresis /copyright /ordfeminine /guillemotleft /logicalnot /hyphen /registered /macron
-/degree /plusminus /twosuperior /threesuperior /acute /mu /paragraph /periodcentered
-/cedilla /onesuperior /ordmasculine /guillemotright /onequarter /onehalf /threequarters /questiondown
-/Agrave /Aacute /Acircumflex /Atilde /Adieresis /Aring /AE /Ccedilla
-/Egrave /Eacute /Ecircumflex /Edieresis /Igrave /Iacute /Icircumflex /Idieresis
-/Eth /Ntilde /Ograve /Oacute /Ocircumflex /Otilde /Odieresis /multiply
-/Oslash /Ugrave /Uacute /Ucircumflex /Udieresis /Yacute /Thorn /germandbls
-/agrave /aacute /acircumflex /atilde /adieresis /aring /ae /ccedilla
-/egrave /eacute /ecircumflex /edieresis /igrave /iacute /icircumflex /idieresis
-/eth /ntilde /ograve /oacute /ocircumflex /otilde /odieresis /divide
-/oslash /ugrave /uacute /ucircumflex /udieresis /yacute /thorn /ydieresis]
-/Encoding defineresource pop
-% vim:ff=unix:
-%%EOF
-%%EndDocument
-%%EndResource
-%%EndProlog
-%%BeginSetup
-595 842 0 sps
-1 nc
-T F dt
-T c
-%%IncludeResource: font Courier
-/_F0 /VIM-latin1 /Courier ref
-/F0 10 /_F0 ffs
-%%IncludeResource: font Courier-Bold
-/_F1 /VIM-latin1 /Courier-Bold ref
-/F1 10 /_F1 ffs
-%%IncludeResource: font Courier-Oblique
-/_F2 /VIM-latin1 /Courier-Oblique ref
-/F2 10 /_F2 ffs
-%%IncludeResource: font Courier-BoldOblique
-/_F3 /VIM-latin1 /Courier-BoldOblique ref
-/F3 10 /_F3 ffs
-/UO -1 d
-/UW 0.5 d
-/BO -2.5 d
-%%EndSetup
-%%Page: 1 1
-%%BeginPageSetup
-sv
-0 g
-F0 sf
-%%EndPageSetup
-F1 sf
-(impact_effort_scatter_plot.py                                                 Page 1)59.5 792.4 ms
-F0 sf
-0 0.498 0.498 r
-(#! /usr/bin/env python3)59.5 772.4 ms
-(# # Impact v. effort scatter plot)59.5 752.4 ms
-(# ## In brevi)59.5 732.4 ms
-(#)59.5 722.4 ms
-(# It is a grid or matrix to help in deciding which things to work on. It focuses on )59.5 712.4 ms
-(the impact of doing something v. the effort required.)59.5 702.4 ms
-(# ## Data)59.5 682.4 ms
-(#)59.5 672.4 ms
-(# Download the data file:)59.5 662.4 ms
-(#)59.5 652.4 ms
-(# - [Impact v. Effort]\(https://drive.google.com/open?id=0BzrdQfHR2I5DMFZVVG1TMnhWOFU)59.5 642.4 ms
-(\))59.5 632.4 ms
-(# ## Methodology)59.5 612.4 ms
-(#)59.5 602.4 ms
-(# An impact v. effort grid is drawn using a scatter plot with pandas.DataFrame.plot.)59.5 592.4 ms
-(scatter. Points are annotated with matplotlib.axes.Axes.annotate. The 'grid' is crea)59.5 582.4 ms
-(ted with matplotlib.axes.Axes.axvline and matplotlib.axes.Axes.avhline.)59.5 572.4 ms
-0.251 0.251 0 r
-(import)59.5 552.4 ms
-0 g
-( pandas )s
-F1 sf
-0 0 0.376 r
-(as)s
-F0 sf
-0 g
-( pd)s
-0.251 0.251 0 r
-(import)59.5 542.4 ms
-0 g
-( matplotlib.pyplot )s
-F1 sf
-0 0 0.376 r
-(as)s
-F0 sf
-0 g
-( plt)s
-(impact_effort = pd.read_csv\('impact_effort.csv'\))59.5 522.4 ms
-(title = 'Impact v. Effort')59.5 502.4 ms
-(subtitle = 'Symphony Processes')59.5 492.4 ms
-(yaxislabel = 'Impact')59.5 482.4 ms
-(xaxislabel = 'Effort')59.5 472.4 ms
-(ax = impact_effort.plot.scatter\(x='effort', y='impact', marker='o', legend=)59.5 452.4 ms
-0.251 0.251 0 r
-(False)s
-0 g
-(\))s
-F1 sf
-0 0 0.376 r
-(for)59.5 442.4 ms
-F0 sf
-0 g
-( spine )s
-0 0 0.376 r
-(in)s
-0 g
-( 'right', 'top':)s
-(    ax.spines[spine].set_color\('none'\))59.5 432.4 ms
-(ax.set_title\(title + ')59.5 422.4 ms
-0 0 0.376 r
-(\\n)s
-0 g
-(' + subtitle, fontweight="bold"\))s
-(ax.set_ylabel\(yaxislabel\))59.5 412.4 ms
-(ax.set_xlabel\(xaxislabel\))59.5 402.4 ms
-F1 sf
-0 0 0.376 r
-(for)59.5 392.4 ms
-F0 sf
-0 g
-( i, txt )s
-0 0 0.376 r
-(in)s
-0 g
-( )s
-0.251 0.251 0 r
-(enumerate)s
-0 g
-(\( impact_effort.process \):)s
-(    ax.annotate\( txt, \( impact_effort.effort[i] + )59.5 382.4 ms
-0.376 g
-(1)s
-0 g
-(, impact_effort.impact[i] + )s
-0.376 g
-(1)s
-0 g
-( \) \))s
-(ax.set_ylim\()59.5 372.4 ms
-0.376 g
-(0)s
-0 g
-(, )s
-0.376 g
-(100)s
-0 g
-(\))s
-(ax.set_xlim\()59.5 362.4 ms
-0.376 g
-(0)s
-0 g
-(, )s
-0.376 g
-(100)s
-0 g
-(\))s
-0 0.498 0.498 r
-(# Plot horizontal reference line)59.5 352.4 ms
-0 g
-(ax.axhline\(y=)59.5 342.4 ms
-0.376 g
-(50)s
-0 g
-(, color='r'\))s
-0 0.498 0.498 r
-(# Plot vertical reference line)59.5 332.4 ms
-0 g
-(ax.axvline\(x=)59.5 322.4 ms
-0.376 g
-(50)s
-0 g
-(, color='r'\))s
-(ax.figure.savefig\('impact_effort.svg', )59.5 312.4 ms
-0.251 0.251 0 r
-(format)s
-0 g
-(='svg'\))s
-(ax.figure.savefig\('impact_effort.pdf', )59.5 302.4 ms
-0.251 0.251 0 r
-(format)s
-0 g
-(='pdf'\))s
-(ax.figure.savefig\('impact_effort.png', )59.5 292.4 ms
-0.251 0.251 0 r
-(format)s
-0 g
-(='png'\))s
-0 0.498 0.498 r
-(# ## References)59.5 272.4 ms
-(#)59.5 262.4 ms
-(# American Society for Quality. "Impact Effort Matrix." [\(http://asq.org/healthcare-)59.5 252.4 ms
-(use/why-quality/impact-effort.html\)]\(http://asq.org/healthcare-use/why-quality/impac)59.5 242.4 ms
-(t-effort.html\). Accessed 2017-08-08.)59.5 232.4 ms
-(#)59.5 222.4 ms
-(# Health Quality Ontario. "Impact/Effort Decision Making Grid." [\(http://www.hqontar)59.5 212.4 ms
-(io.ca/Portals/0/documents/qi/learningcommunity/pc-impact-effort-decision-making-crit)59.5 202.4 ms
-(eria-chronic-disease-roadmap-resource-en.pdf\)]\(http://www.hqontario.ca/Portals/0/doc)59.5 192.4 ms
-(uments/qi/learningcommunity/pc-impact-effort-decision-making-criteria-chronic-diseas)59.5 182.4 ms
-(e-roadmap-resource-en.pdf\). Accessed 2017-08-08.)59.5 172.4 ms
-re sp
-%%PageTrailer
-%%Trailer
-%%Pages: 1
-%%EOF
-
+#! /usr/bin/env python3
+
+# # Impact v. effort scatter plot
+
+# ## In brevi
+#
+# It is a grid or matrix to help in deciding which things to work on. It focuses on the impact of doing something v. the effort required.
+
+# ## Data
+#
+# Download the data file:
+#
+# - [Impact v. Effort](https://drive.google.com/open?id=0BzrdQfHR2I5DMFZVVG1TMnhWOFU)
+
+# ## Methodology
+#
+# An impact v. effort grid is drawn using a scatter plot with pandas.DataFrame.plot.scatter. Points are annotated with matplotlib.axes.Axes.annotate. The 'grid' is created with matplotlib.axes.Axes.axvline and matplotlib.axes.Axes.avhline.
+
+# In[1]:
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+impact_effort = pd.read_csv('impact_effort.csv')
+
+title = 'Impact v. Effort'
+subtitle = 'Symphony Processes'
+yaxislabel = 'Impact'
+xaxislabel = 'Effort'
+
+ax = impact_effort.plot.scatter(x='effort', y='impact', marker='o', legend=False)
+for spine in 'right', 'top':
+    ax.spines[spine].set_color('none')
+
+ax.set_title(title + '\n' + subtitle, fontweight="bold")
+ax.set_ylabel(yaxislabel)
+ax.set_xlabel(xaxislabel)
+
+for i, txt in enumerate( impact_effort.process ):
+    ax.annotate( txt, ( impact_effort.effort[i] + 1, impact_effort.impact[i] + 1 ) )
+
+ax.set_ylim(0, 100)
+ax.set_xlim(0, 100)
+# Plot horizontal reference line
+ax.axhline(y=50, color='r')
+# Plot vertical reference line
+ax.axvline(x=50, color='r')
+ax.figure.savefig('impact_effort.svg', format='svg')
+ax.figure.savefig('impact_effort.pdf', format='pdf')
+ax.figure.savefig('impact_effort.png', format='png')
+
+
+# ## References
+#
+# American Society for Quality. "Impact Effort Matrix." [(http://asq.org/healthcare-use/why-quality/impact-effort.html)](http://asq.org/healthcare-use/why-quality/impact-effort.html). Accessed 2017-08-08.
+#
+# Health Quality Ontario. "Impact/Effort Decision Making Grid." [(http://www.hqontario.ca/Portals/0/documents/qi/learningcommunity/pc-impact-effort-decision-making-criteria-chronic-disease-roadmap-resource-en.pdf)](http://www.hqontario.ca/Portals/0/documents/qi/learningcommunity/pc-impact-effort-decision-making-criteria-chronic-disease-roadmap-resource-en.pdf). Accessed 2017-08-08.
