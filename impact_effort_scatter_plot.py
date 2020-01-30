ee9eace922ee61876ed44a7e47a0b13f36bf126a
Gilles Pilon <gillespilon13@gmail.com>
2020-01-30 15:53:13 -0600

    Add if --name-- ..., def main, move all relevant code to within def main

--- impact_effort_scatter_plot.py
+++ impact_effort_scatter_plot.py
@@ -22,28 +22,33 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-title = 'Impact versus effort'
-subtitle = 'Potential controls'
-yaxislabel = 'Impact'
-xaxislabel = 'Effort'
-
 c = cm.Paired.colors
 
-impact_effort = pd.read_csv('impact_effort.csv')
-ax = impact_effort.plot.scatter(x='effort', y='impact', marker='o',
-                                color=c[0], legend=False)
-for spine in 'right', 'top':
-    ax.spines[spine].set_visible(False)
-ax.set_title(title + '\n' + subtitle, fontweight="bold")
-ax.set_ylabel(yaxislabel, fontweight="bold")
-ax.set_xlabel(xaxislabel, fontweight="bold")
-for i, txt in enumerate(impact_effort.process):
-    ax.annotate(txt, (impact_effort.effort[i] + 1,
-                      impact_effort.impact[i] + 1))
-ax.set_ylim(0, 100)
-ax.set_xlim(0, 100)
-ax.axhline(y=50, color=c[1])
-ax.axvline(x=50, color=c[1])
-ax.figure.savefig('impact_effort.svg', format='svg')
-ax.figure.savefig('impact_effort.pdf', format='pdf')
-ax.figure.savefig('impact_effort.png', format='png')
+
+def main():
+    title = 'Impact versus effort'
+    subtitle = 'Potential controls'
+    yaxislabel = 'Impact'
+    xaxislabel = 'Effort'
+    impact_effort = pd.read_csv('impact_effort.csv')
+    ax = impact_effort.plot.scatter(x='effort', y='impact', marker='o',
+                                    color=c[0], legend=False)
+    for spine in 'right', 'top':
+        ax.spines[spine].set_visible(False)
+    ax.set_title(title + '\n' + subtitle, fontweight="bold")
+    ax.set_ylabel(yaxislabel, fontweight="bold")
+    ax.set_xlabel(xaxislabel, fontweight="bold")
+    for i, txt in enumerate(impact_effort.process):
+        ax.annotate(txt, (impact_effort.effort[i] + 1,
+                          impact_effort.impact[i] + 1))
+    ax.set_ylim(0, 100)
+    ax.set_xlim(0, 100)
+    ax.axhline(y=50, color=c[1])
+    ax.axvline(x=50, color=c[1])
+    ax.figure.savefig('impact_effort.svg', format='svg')
+    ax.figure.savefig('impact_effort.pdf', format='pdf')
+    ax.figure.savefig('impact_effort.png', format='png')
+
+
+if __name__ == '__main__':
+    main()
